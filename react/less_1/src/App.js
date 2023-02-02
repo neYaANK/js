c8f84c9c343b69bcbd05ee7a16a81b0b373a89b8
Artem Nersesian <hotmail.noob1109@gmail.com>
2023-02-02 23:49:44 +0100

    react less1

---
+++ react/less_1/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+
+function App() {
+  /*  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+    */
+  return React.createElement("div", { className: "App" },
+    React.createElement("header", { className: "App-header" },
+      React.createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      React.createElement("p", null,
+        "Edit",
+        React.createElement("code", null, "src/App.js"),
+        " and save to reload."),
+      React.createElement("a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer"
+        },
+        "Learn React")
+    ));
+}
+
+export default App;
---
+++ react/less_1/src/components/UserCard/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+class UserCard extends React.Component {
+    render() {
+        return (
+            <article>
+                <h2>John Doe</h2>
+                <img src="https://www.portablepress.com/wp-content/uploads/2014/02/John-Doe_text.jpg"></img>
+            </article>
+        )
+    }
+}
+export default UserCard
